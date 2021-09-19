1083110cd5042fb46c9c54c2ec0b8ce3994aa048
Gianna Torpey <gianna.torpey@gmail.com>
2021-09-19 01:55:28 -0400

    Created image grid with sample images

--- frontend/src/EventPage.js
+++ frontend/src/EventPage.js
@@ -1,8 +1,31 @@
 import React from "react";
 import "./EventPage.css";
+import Gallery from 'react-grid-gallery';
 
 
 function EventPage(props){
+    const IMAGES =
+[{
+        src: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+        thumbnail: "https://c2.staticflickr.com/9/8817/28973449265_07e3aa5d2e_b.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 174,
+        caption: "After Rain (Jeshu John - designerspics.com)"
+},
+{
+        src: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+        thumbnail: "https://c2.staticflickr.com/9/8356/28897120681_3b2c0f43e0_b.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 212,
+        caption: "Boats (Jeshu John - designerspics.com)"
+},
+ 
+{
+        src: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
+        thumbnail: "https://c4.staticflickr.com/9/8887/28897124891_98c4fdd82b_b.jpg",
+        thumbnailWidth: 320,
+        thumbnailHeight: 212
+}]
     return (
         <div className="everything">
             <div className="goBack">
@@ -17,6 +40,7 @@ function EventPage(props){
             <div className="addPost">
                 <button>Add Post</button>
             </div>
+            <Gallery images={IMAGES}/>
         </div>
     )
 }
